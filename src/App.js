ff7c683d08fd1c99722844513cd983beb91bddb1
Deepak Rai <Deepakrai767622@gmail.com>
2023-12-18 18:34:58 +0530

    update
    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ReactGA from "react-ga";
 import $ from "jquery";
-import "./App.css";
+import "./app.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import About from "./Components/About";
